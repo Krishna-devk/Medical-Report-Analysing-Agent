14264c286c8cbfb8393bc3d4e768b36792ce5b10
Krishna-devk <goldyagarwal81@gmail.com>
2026-02-24 20:58:39 +0530

    done

--- main.py
+++ main.py
@@ -1,21 +1,5 @@
-from fastapi import FastAPI
-from fastapi.middleware.cors import CORSMiddleware
+import uvicorn
+from app import app
 
-from config import config
-from routes import router as MedRouter
-
-
-app = FastAPI(
-    title="QuickCare AI",
-    description="AI-powered medical report analysis using Google Gemini",
-    version="1.0.0",
-)
-
-app.add_middleware(
-    CORSMiddleware,
-    allow_origins=["*"],
-    allow_methods=["*"],
-    allow_headers=["*"],
-)
-
-app.include_router(MedRouter)
+if __name__ == "__main__":
+    uvicorn.run("main:app", host="0.0.0.0", port=8000, reload=True)
